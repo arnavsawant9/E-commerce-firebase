1a568bd9e95b5da204d1e36ffcf0270ac5ad6c5b
arnavsawant9 <arnavsawant2004@gmail.com>
2024-12-11 12:03:29 +0530

    basic setup initialized

---
+++ client/src/firebase.config.js
@@ -0,0 +1,36 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCZbqeBEv3wGhHXbzTm3ardrrDMv0HH1_s",
+//   authDomain: "e-commerce-firebase-f4be8.firebaseapp.com",
+//   projectId: "e-commerce-firebase-f4be8",
+//   storageBucket: "e-commerce-firebase-f4be8.firebasestorage.app",
+//   messagingSenderId: "503778132126",
+//   appId: "1:503778132126:web:ceec6a10423b996c053b8f",
+//   measurementId: "G-BZ2KMDD081"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+
+// actual code here!!
+const firebaseConfig = {
+    apiKey: "AIzaSyCZbqeBEv3wGhHXbzTm3ardrrDMv0HH1_s",
+    authDomain: "e-commerce-firebase-f4be8.firebaseapp.com",
+    projectId: "e-commerce-firebase-f4be8",
+    storageBucket: "e-commerce-firebase-f4be8.firebasestorage.app",
+    messagingSenderId: "503778132126",
+    appId: "1:503778132126:web:ceec6a10423b996c053b8f",
+    measurementId: "G-BZ2KMDD081"
+  };
+
+export default firebaseConfig;
